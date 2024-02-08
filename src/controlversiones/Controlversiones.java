d488d9c7d035591bbb703cdc6308fe15374dfe1f
dam <dam@203P22>
2024-02-08 09:13:37 +0100

    Segunda confirmacion

--- src/controlversiones/Controlversiones.java
+++ src/controlversiones/Controlversiones.java
@@ -15,6 +15,7 @@ public class Controlversiones {
      */
     public static void main(String[] args) {
         System.out.println("hola a todos");
+        System.out.println("adios a todos");
     }
     
 }
